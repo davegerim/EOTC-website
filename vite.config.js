63cc5371348251439602ec748e4fda2ca222846c
Your Name <your.email@example.com>
2025-12-11 13:55:05 +0300

    added config file for deployment

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                works: resolve(__dirname, 'works.html'),
+                heritage: resolve(__dirname, 'heritage.html'),
+            },
+        },
+    },
+});
